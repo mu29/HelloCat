c273d489b7416be24fcca1f955e9b9043d372cfa
InJung Chung <mu29@yeoubi.net>
2017-02-25 21:52:41 +0900

    Put logo at navigation

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import { Platform, View, StyleSheet } from 'react-native';
+import { Platform, Image, Text, View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     height: 48 + (Platform.OS === 'android' ? 0 : 20),
+    paddingTop: Platform.OS === 'android' ? 0 : 20,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FFF',
     borderBottomColor: '#E0E0E0',
     borderBottomWidth: 0.5,
+  },
+  title: {
+    marginLeft: 8,
+    color: "#5B93FC",
+    fontSize: 18,
+    fontWeight: '500',
   },
 });
 
@@ -26,6 +33,8 @@ export default class Navigation extends Component {
   render() {
     return (
       <View style={ styles.container }>
+        <Image source={ require('../resources/icon.png') } style={ { width: 24, height: 24 } } />
+        <Text style={ styles.title }>DailyCat</Text>
       </View>
     );
   }
