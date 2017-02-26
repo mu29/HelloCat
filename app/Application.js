f6b12a06632d796f83bbd85db3ca038f3c2c77ae
InJung Chung <mu29@yeoubi.net>
2017-02-26 18:41:55 +0900

    Add Firebase configuration

--- app/Application.js
+++ app/Application.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { AppState, Platform, View, Navigator, StyleSheet } from 'react-native';
+import * as firebase from 'firebase';
 import configureStore from './store/configureStore';
 import Router from './Router';
+
+firebase.initializeApp({
+  apiKey: "<API_KEY>",
+  authDomain: "<PROJECT_ID>.firebaseapp.com",
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  storageBucket: "<BUCKET>.appspot.com",
+  messagingSenderId: "<SENDER_ID>",
+});
 
 export default class Application extends React.Component {
   constructor() {
